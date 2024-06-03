aeb971204354f06839ed907bc7c59759b8994477
roger-tars <roger.camposjr90@gmail.com>
2024-06-03 20:13:44 -0300

    build package json

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
